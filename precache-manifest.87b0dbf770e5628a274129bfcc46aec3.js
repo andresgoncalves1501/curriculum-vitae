2465a2bd95aa01e89cbe5302147f5ff483787940
André Gonçalves <andresg1501@gmail.com>
2020-09-17 21:06:50 +0100

    commit cv

---
+++ precache-manifest.87b0dbf770e5628a274129bfcc46aec3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8ed49c9eca94f7efb1",
+    "url": "/curriculum-vitae/css/app.60a6731b.css"
+  },
+  {
+    "revision": "bbe87caa83133a567351239b2085519a",
+    "url": "/curriculum-vitae/img/andre.bbe87caa.png"
+  },
+  {
+    "revision": "50727115d93a20fa5d1bc2cfe9ccd53c",
+    "url": "/curriculum-vitae/index.html"
+  },
+  {
+    "revision": "ec8ed49c9eca94f7efb1",
+    "url": "/curriculum-vitae/js/app.0d2af6ce.js"
+  },
+  {
+    "revision": "132a4ef9d91a131526a4",
+    "url": "/curriculum-vitae/js/chunk-vendors.a8b9f199.js"
+  },
+  {
+    "revision": "b86ee9d79e84e8a95bad050900aaec1e",
+    "url": "/curriculum-vitae/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/curriculum-vitae/robots.txt"
+  }
+]);
